d6d2586134997a24b6709b275687c326c7a17ef1
zhangruochi <zrc720@gmail.com>
2019-12-04 11:37:25 -0500

    2019-12-04

--- embedding/word2vec.py
+++ embedding/word2vec.py
@@ -1,4 +1,5 @@
 import gensim.downloader as api
+import numpy as np
 
 def load_word2vec(word2vec_file = "word2vec-google-news-300"):
     """ Load Word2Vec Vectors
@@ -12,7 +13,7 @@ def load_word2vec(word2vec_file = "word2vec-google-news-300"):
 
 
 
-def get_matrix_of_vectors(wv_from_bin, required_words=['barrels', 'bpd', 'ecuador', 'energy', 'industry', 'kuwait', 'oil', 'output', 'petroleum', 'venezuela']):
+def get_matrix_of_vectors(wv_from_bin, required_words):
     """ Put the word2vec vectors into a matrix M.
         Param:
             wv_from_bin: KeyedVectors object; the 3 million word2vec vectors loaded from file
@@ -47,4 +48,63 @@ def get_matrix_of_vectors(wv_from_bin, required_words=['barrels', 'bpd', 'ecuado
     print("Done.")
     return M, word2Ind
 
+
+def display_pca_scatterplot(model, words=None, sample=0):
+    if words == None:
+        if sample > 0:
+            words = np.random.choice(list(model.vocab.keys()), sample)
+        else:
+            words = [ word for word in model.vocab ]
+        
+    word_vectors = np.array([model[w] for w in words])
+
+    twodim = PCA().fit_transform(word_vectors)[:,:2]
     
+    plt.figure(figsize=(6,6))
+    plt.scatter(twodim[:,0], twodim[:,1], edgecolors='k', c='r')
+    for word, (x,y) in zip(words, twodim):
+        plt.text(x+0.05, y+0.05, word)
+
+
+
+def compute_co_occurrence_matrix(corpus, window_size=4):
+    """ Compute co-occurrence matrix for the given corpus and window_size (default of 4).
+    
+        Note: Each word in a document should be at the center of a window. Words near edges will have a smaller
+              number of co-occurring words.
+              
+              For example, if we take the document "START All that glitters is not gold END" with window size of 4,
+              "All" will co-occur with "START", "that", "glitters", "is", and "not".
+    
+        Params:
+            corpus (list of list of strings): corpus of documents
+            window_size (int): size of context window
+        Return:
+            M (numpy matrix of shape (number of corpus words, number of corpus words)): 
+                Co-occurence matrix of word counts. 
+                The ordering of the words in the rows/columns should be the same as the ordering of the words given by the distinct_words function.
+            word2Ind (dict): dictionary that maps word to index (i.e. row/column number) for matrix M.
+    """
+    words, num_words = distinct_words(corpus)
+    M = None
+    word2Ind = {}
+
+    
+    # ------------------
+    # Write your implementation here.
+    
+    word2Ind = {word:idx for word,idx in zip(words, range(num_words))}
+    M = np.zeros((num_words,num_words))
+    
+    for doc in corpus:
+        for index, central_word in enumerate(doc):
+            left = max(0,index - window_size)
+            right = min(num_words, index + window_size+1)
+            for context_word in doc[left:right]:
+                if context_word == central_word:
+                    continue
+                M[word2Ind[central_word]][word2Ind[context_word]] += 1
+
+    # ------------------
+
+    return M, word2Ind
